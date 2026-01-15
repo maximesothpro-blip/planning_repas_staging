64b446f15030f29f963d9dd541d05ed8319ef749
maximesothpro-blip <maximesothpro@gmail.com>
2026-01-15 14:52:24 -0500

    Update BACKEND_API_URL to staging environment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration de l'URL du backend
 // URL Tunnel Permanent Cloudflare avec domaine personnalisé
-window.BACKEND_API_URL = 'https://api.msnew.cloud';
+window.BACKEND_API_URL = 'https://api-staging.msnew.cloud';
 
 // Note: n8n webhook URL is now configured in backend server.js (v3.10)
